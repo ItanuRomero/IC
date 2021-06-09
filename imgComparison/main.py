6f80393bc8a6d61562dcceb38e9e718f776d4693
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-06-09 09:22:50 -0300

    imgComparison change

--- imgComparison/main.py
+++ imgComparison/main.py
@@ -5,5 +5,9 @@ first_image, second_image = cv2.imread(first_image), cv2.imread(second_image)
 diff = cv2.subtract(first_image, second_image)
 if diff.any():
     print("As imagens são diferentes")
+    for vetor in diff:
+        for v in vetor:
+            if v.all():
+                print(vetor)
 else:
     print("As imagens são iguais")
