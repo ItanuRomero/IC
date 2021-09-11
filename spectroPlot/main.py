b5b9b4808b70148fe29c598ede1d6e73df3e2658
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-09-11 15:37:15 -0300

    improve some changes on spectroplot/main.py
    With the changes now we can see the image with some information but the saved image is pure and simple spectrum

--- spectroPlot/main.py
+++ spectroPlot/main.py
@@ -2,17 +2,31 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-# sr -> frequência de amostragem
-file_name = 'cardio.mp3'
-data, fs = librosa.load(file_name, sr=44100)
-plt.figure(figsize=(12, 5))
-D = librosa.amplitude_to_db(np.abs(librosa.stft(data)))
-librosa.display.specshow(D, x_axis='time', y_axis='linear', sr=fs, cmap='CMRmap')
-plt.xlabel('Time [s]')
-plt.ylabel('Frequency [Hz]')
-# remove other information
-plt.xticks([], [])
-plt.yticks([], [])
-plt.colorbar(format='%+2.0f dB')
-plt.savefig(f'{file_name[:-4]}.png', format='png')
-plt.show()
+
+def create_spectrum(file_name):
+    # sr -> sampling frequency
+    data, fs = librosa.load(file_name, sr=44100)
+    plt.figure(figsize=(12, 5))
+    processed_data = librosa.amplitude_to_db(np.abs(librosa.stft(data)))
+    librosa.display.specshow(processed_data, x_axis='time', y_axis='linear', sr=fs, cmap='CMRmap')
+    save_spectrum_image(file_name)
+    show_spectrum_image()
+
+
+def save_spectrum_image(file_name):
+    plt.axis('off')
+    # maybe we can remove all black pixels with opencv
+    plt.savefig(f'{file_name[:-4]}.png', format='png', bbox_inches='tight', transparent="True", pad_inches=0)
+
+
+def show_spectrum_image():
+    plt.axis('on')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Frequency [Hz]')
+    plt.xticks([], [])
+    plt.yticks([], [])
+    plt.colorbar(format='%+2.0f dB')
+    plt.show()
+
+
+create_spectrum(file_name='cardio.mp3')
